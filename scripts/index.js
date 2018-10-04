7064c9121122a15f839c7c826bba82aa4937813d
Isaac Fonseca <isaacfo1010@gmail.com>
2018-10-04 15:08:56 -0400

    added some js syntax exs

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+//Yep. It's JavaScript
+
+console.log('Hello JavaScript, what is up.')
+
+//hello.py
+//Getting input
+
+
+//String interpolation
+// and camelCase
+// var promptText =('What is your name?');
+// var userName = prompt(promptText);
+
+
+// //uses string concatenation
+// var greeting = 'Hello ' + userName + '!';
+// console.log(greeting);
+
+
+// using let and const
+// Rule of thumb: use const until you get an error
+const promptText = ('What is your name?');
+let userName = prompt(promptText);
+userName = userName + ' is cool';
+
+
+// string interpolation
+// 1. Replace quotes with brackets 
+// 2. Remove the + signs
+// 3. Wrap your variables in ${} 
+const greeting = `Hello , ${userName}!`;
+console.log(greeting)
+
+
+
+
--- scripts/scripts.index.js
+++
@@ -1,1 +0,0 @@
-//Yep. It's JavaScript
